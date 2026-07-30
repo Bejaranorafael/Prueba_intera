e66f48f2f9b6c792fd7533e9d074fc4ae90c9775
Rafael Bejarano <40178689+Bejaranorafael@users.noreply.github.com>
2026-07-30 17:42:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,13 @@ const foto=document.getElementById("photo");
 
 const historia=document.getElementById("story");
 
+
 boton.onclick=()=>{
 
 boton.style.display="none";
 
 terminal.style.display="block";
+
 
 let texto=
 
@@ -34,9 +36,11 @@ Se encontró un desarrollador
 que no puede dejar de pensar
 en vos...`;
 
+
 let i=0;
 
 terminal.innerHTML="";
+
 
 let maquina=setInterval(()=>{
 
@@ -44,15 +48,66 @@ terminal.innerHTML+=texto.charAt(i);
 
 i++;
 
+
 if(i==texto.length){
 
 clearInterval(maquina);
+
 
 setTimeout(()=>{
 
 terminal.style.display="none";
 
 foto.style.display="block";
+
+
+// Nueva escena
+
+setTimeout(()=>{
+
+historia.style.display="block";
+
+let mensaje=
+
+`Dicen que en algún lugar del océano
+existe una sirena que guarda un secreto...
+
+Pero este secreto no está en el mar.
+
+Está en una sonrisa,
+en una mirada,
+en esos pequeños momentos
+que hacen que un día común
+se vuelva especial.
+
+Sol, esta pequeña aventura
+es solo una forma de recordarte
+lo increíble que sos ❤️`;
+
+
+let j=0;
+
+historia.innerHTML="";
+
+
+let escribir=setInterval(()=>{
+
+historia.innerHTML+=mensaje.charAt(j);
+
+j++;
+
+
+if(j==mensaje.length){
+
+clearInterval(escribir);
+
+}
+
+},45);
+
+
+},2500);
+
 
 },1800);
 
